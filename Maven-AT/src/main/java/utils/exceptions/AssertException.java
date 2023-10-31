a64f134082ebb77d27ac8198b9f1affb49b2f836
Nikolay Mishin <nikmarqa@gmail.com>
2023-10-31 13:51:48 +0300

    Add AssertException

---
+++ Maven-AT/src/main/java/utils/exceptions/AssertException.java
@@ -0,0 +1,37 @@
+package utils.exceptions;
+
+public class AssertException {
+
+    private final Object value;
+
+    public AssertException(Object value) {
+        this.value = value;
+    }
+
+    private void _assert(Object value, Object compare, Boolean equals) {
+        assert equals == (value == compare);
+    }
+
+    private void _assert(Object compare, Boolean equals) {
+        _assert(value, compare, equals);
+    }
+
+    private void notNull(Object value) {
+        _assert(value, null, false);
+    }
+
+    public void notNull() throws NullPointerException {
+        notNull(value);
+    }
+
+    public void  _equals(Object compare) {
+        notNull(compare);
+        _assert(compare, true);
+    }
+
+    public void notEquals(Object compare) {
+        notNull(compare);
+        _assert(compare, false);
+    }
+
+}
