d15254015cf1452856fce7dbb1453b219f890241
Nikolay Mishin <nikmarqa@gmail.com>
2023-11-04 16:49:19 +0300

    Refactor Auth

--- Maven-AT/src/main/java/utils/base/Auth.java
+++ Maven-AT/src/main/java/utils/base/Auth.java
@@ -6,8 +6,14 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Auth extends SingleInstance<Auth> {
 
-    public Auth() {
-        //Auth.instance();
+    private String token;
+    private String fileToken;
+    private String refreshToken;
+
+    public Auth(String token, String fileToken, String refreshToken) {
+        this.token = token;
+        this.fileToken = fileToken;
+        this.refreshToken = refreshToken;
     }
 
     public static Auth instance() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
