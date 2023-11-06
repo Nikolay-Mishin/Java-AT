def1e1ee03655deb51176de09e530e7b5f5f7cc1
Nikolay Mishin <nikmarqa@gmail.com>
2023-11-06 18:03:21 +0300

    Add IConfig

--- Maven-AT/src/main/java/config/WebConfig.java
+++ Maven-AT/src/main/java/config/WebConfig.java
@@ -1,39 +1,13 @@
 package config;
 
 import org.aeonbits.owner.Config;
-import static org.aeonbits.owner.ConfigFactory.*;
+import utils.config.IConfig;
+
+import static utils.config.Config.setConfig;
 
 @Config.Sources({"classpath:${env}.properties"})
-public interface WebConfig extends Config {
+public interface WebConfig extends IConfig {
 
-    WebConfig BASE_CONFIG = create(WebConfig.class, System.getenv(), System.getProperties());
-
-    //at
-    @Key("BASE_URL")
-    String getBaseUrl();
-
-    @Key("USER_LOGIN")
-    String getUserLogin();
-
-    @Key("USER_PASSWORD")
-    String getUserPassword();
-
-    //db
-    @Key("DB_URL")
-    String getDbUrl();
-
-    @Key("DB_USER_NAME")
-    String getDbLogin();
-
-    @Key("DB_USER_PASS")
-    String getDbPassword();
-
-    //resources
-    @Key("JSON_SCHEMA_ROOT")
-    String getJsonSchemaRoot();
-
-    //tokens
-    @Key("TOKEN_KEYS")
-    String getTokenKeys();
+    WebConfig BASE_CONFIG = setConfig(WebConfig.class);
 
 }
---
+++ Maven-AT/src/main/java/utils/config/IConfig.java
@@ -0,0 +1,40 @@
+package utils.config;
+
+import org.aeonbits.owner.Config;
+
+import static utils.config.Config.setConfig;
+
+@org.aeonbits.owner.Config.Sources({"classpath:${env}.properties"})
+public interface IConfig extends Config {
+
+    IConfig BASE_CONFIG = setConfig(IConfig.class);
+
+    //at
+    @Key("BASE_URL")
+    String getBaseUrl();
+
+    @Key("USER_LOGIN")
+    String getUserLogin();
+
+    @Key("USER_PASSWORD")
+    String getUserPassword();
+
+    //db
+    @Key("DB_URL")
+    String getDbUrl();
+
+    @Key("DB_USER_NAME")
+    String getDbLogin();
+
+    @Key("DB_USER_PASS")
+    String getDbPassword();
+
+    //resources
+    @Key("JSON_SCHEMA_ROOT")
+    String getJsonSchemaRoot();
+
+    //tokens
+    @Key("TOKEN_KEYS")
+    String getTokenKeys();
+
+}
