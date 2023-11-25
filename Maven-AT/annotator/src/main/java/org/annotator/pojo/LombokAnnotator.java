6d137d3ffa3be2be95204a8b642207f6ff39e0d4
Nikolay Mishin <nikmarqa@gmail.com>
2023-11-25 19:02:36 +0300

    Fix Structure

--- Maven-AT/annotator/src/main/java/org/annotator/pojo/LombokAnnotator.java
+++ Maven-AT/annotator/src/main/java/org/annotator/pojo/LombokAnnotator.java
@@ -2,31 +2,42 @@ package org.annotator.pojo;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sun.codemodel.JDefinedClass;
-import lombok.Builder;
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
 import org.jsonschema2pojo.AbstractAnnotator;
 
 import java.lang.annotation.Annotation;
+import java.util.List;
 
 import static java.lang.System.out;
 
 public class LombokAnnotator extends AbstractAnnotator {
 
-    public Class<? extends Annotation> getAnnotation(String property) {
+    private static final List<String> defaultAnnotations = List.of("lombok-builder", "lombok-data");
+
+    private static Class<? extends Annotation> getAnnotation(String property) {
         return switch (property) {
             case "lombok-builder" -> Builder.class;
             case "lombok-data" -> Data.class;
+            case "lombok-getter" -> Getter.class;
+            case "lombok-setter" -> Setter.class;
+            case "lombok-equals-and-hash-code" -> EqualsAndHashCode.class;
+            case "lombok-no-args-constructor" -> NoArgsConstructor.class;
+            case "lombok-all-args-constructor" -> AllArgsConstructor.class;
             case "lombok-to-string" -> ToString.class;
             default -> throw new IllegalStateException("Unexpected value: " + property);
         };
     }
 
-    public void setAnnotation(JDefinedClass clazz, String property) {
+    private static void setAnnotation(JDefinedClass clazz, String property) {
+        out.println(property);
         Class<? extends Annotation> annotation = getAnnotation(property);
         if (!annotation.equals(IllegalStateException.class)) {
             clazz.annotate(annotation);
         }
+    }
+
+    private static void setDefaultAnnotation(JDefinedClass clazz) {
+        defaultAnnotations.forEach(annotation -> setAnnotation(clazz, annotation));
     }
 
     /*@Override
@@ -41,6 +52,7 @@ public class LombokAnnotator extends AbstractAnnotator {
             additionalProperties.fieldNames().forEachRemaining(property -> setAnnotation(clazz, property));
         } catch (NullPointerException e) {
             out.printf("No additionalProperties defined for %s.%n", clazz.fullName());
+            setDefaultAnnotation(clazz);
         }
     }
 
