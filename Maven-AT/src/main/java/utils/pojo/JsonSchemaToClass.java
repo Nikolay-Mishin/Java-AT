0f4c9a64b4c5009cd46b13815555edcf2dd5b1c0
Nikolay Mishin <nikmarqa@gmail.com>
2023-11-28 16:58:51 +0300

    Add pojo

---
+++ Maven-AT/src/main/java/utils/pojo/JsonSchemaToClass.java
@@ -0,0 +1,9 @@
+package utils.pojo;
+
+import java.io.IOException;
+
+public class JsonSchemaToClass extends org.project.pojo.JsonSchemaToClass {
+
+    public JsonSchemaToClass() throws IOException {}
+
+}
