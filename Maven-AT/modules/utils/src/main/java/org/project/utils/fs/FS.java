44c340ea5ada891a9fd72fdc8898396dc1c7c0d6
Nikolay Mishin <nikmarqa@gmail.com>
2023-11-29 21:54:43 +0300

    Refactor PojoGenerator

--- Maven-AT/modules/utils/src/main/java/org/project/utils/fs/FS.java
+++ Maven-AT/modules/utils/src/main/java/org/project/utils/fs/FS.java
@@ -3,10 +3,17 @@ package org.project.utils.fs;
 import jdk.jfr.Description;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import static java.lang.String.join;
 import static java.lang.System.out;
+import static java.util.Objects.requireNonNull;
 import static org.project.utils.Helper.isInstance;
 
 public class FS {
@@ -19,7 +26,7 @@ public class FS {
             .toArray(String[]::new));
     }
 
-    private static String readFromInputStream(InputStream inputStream) throws IOException {
+    protected static String readFromInputStream(InputStream inputStream) throws IOException {
         StringBuilder resultStringBuilder = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
@@ -35,6 +42,56 @@ public class FS {
         String data = readFromInputStream(inputStream);
         out.println(data);
         return data;
+    }
+
+    public static Stream<Path> readDir(final String path) throws IOException {
+        return readDir(path, Files::isDirectory);
+    }
+
+    public static Stream<Path> listFiles(final String path) throws IOException {
+        return readDir(path, Files::isRegularFile);
+    }
+
+    protected static Stream<Path> readDir(final String path, Predicate<? super Path> filter) throws IOException {
+        out.println(path);
+        Path _path = Paths.get(path);
+        try (final Stream<Path> paths = Files.walk(_path)) {
+            try (final Stream<Path> paths1 = Files.walk(_path)) {
+                Stream<Path> _paths = paths1.filter(filter);
+                List<Path> collect = _paths.toList();
+                out.println(collect);
+                collect.forEach(System.out::println);
+            }
+            try (final Stream<Path> paths2 = Files.walk(_path)) {
+                Stream<Path> _paths = paths2.filter(filter);
+                Stream<File> map = _paths.map(Path::toFile);
+                out.println(map);
+                map.forEach(System.out::println);
+            }
+            try (final Stream<Path> paths3 = Files.walk(_path)) {
+                Stream<Path> _paths = paths3.filter(filter);
+                List<File> map = _paths.map(Path::toFile).toList();
+                out.println(map);
+                map.forEach(System.out::println);
+            }
+            out.println(paths);
+            return paths.filter(filter);
+        }
+    }
+
+    public static void listFilesForFolder(final String path) {
+        listFilesForFolder(new File(path));
+    }
+
+    public static void listFilesForFolder(final File folder) {
+        out.println(folder);
+        for (final File fileEntry : requireNonNull(folder.listFiles())) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                out.println(fileEntry.getPath());
+            }
+        }
     }
 
 }
--- Maven-AT/src/main/java/utils/pojo/PojoGenerator.java
+++ Maven-AT/src/main/java/utils/pojo/PojoGenerator.java
@@ -5,11 +5,52 @@ import org.project.pojo.JsonSchemaToClass;
 import java.io.IOException;
 
 import static config.WebConfig.BASE_CONFIG;
+import static java.lang.System.out;
+import static org.project.utils.fs.FS.*;
 
 public class PojoGenerator {
 
     public static void main(String[] args) throws IOException {
         new JsonSchemaToClass(BASE_CONFIG);
+        String path = BASE_CONFIG.getJsonSchemaRoot();
+        out.println("listFilesForFolder");
+        listFilesForFolder(path);
+        out.println("listFiles");
+        listFiles(path);
+        out.println("readDir");
+        readDir(path);
+        /*readDir(path).forEach(file -> {
+            out.println(file);
+            out.println(file.getClass());
+            out.println(file.toAbsolutePath());
+            try {
+                out.println(file.toRealPath());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            out.println(file.getFileName());
+            out.println(file.getRoot());
+            out.println(file.getParent());
+            out.println(file.getNameCount());
+            out.println(file.getFileSystem());
+            out.println(file.getName(0));
+        });*/
+        /*listFiles(path).forEach(file -> {
+            out.println(file);
+            out.println(file.getClass());
+            out.println(file.toAbsolutePath());
+            try {
+                out.println(file.toRealPath());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            out.println(file.getFileName());
+            out.println(file.getRoot());
+            out.println(file.getParent());
+            out.println(file.getNameCount());
+            out.println(file.getFileSystem());
+            out.println(file.getName(0));
+        });*/
     }
 
 }
