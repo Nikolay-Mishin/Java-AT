87c22ee5c5abca120c5696d9dc969dd925e1a4a2
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-17 16:24:27 +0300

    Refactor ApiConfig

--- modules/utils/src/main/java/org/project/utils/config/ApiConfig.java
+++ modules/utils/src/main/java/org/project/utils/config/ApiConfig.java
@@ -1,14 +1,98 @@
 package org.project.utils.config;
 
+import java.util.Map;
+
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
+import static io.restassured.RestAssured.given;
+
 public class ApiConfig extends Config {
-    public static RequestSpecification requestSpec() {
-        return new RequestSpecBuilder()
-            .setBaseUri(WebConfig.config().getBaseUrl())
+    public static RequestSpecification givenRequestSpec() {
+        return given(getRequestSpec());
+    }
+
+    public static RequestSpecification getRequestSpec() {
+        return requestSpec()
             .addFilter(new AllureRestAssured())
             .build();
+    }
+
+    public static RequestSpecBuilder requestSpecUri(String uri) {
+        return new RequestSpecBuilder().setBaseUri(uri);
+    }
+
+    public static RequestSpecBuilder requestSpec() {
+        return requestSpec(WebConfig.config());
+    }
+
+    public static RequestSpecBuilder requestSpec(WebBaseConfig config) {
+        return requestSpecUri(config.getBaseUrl());
+    }
+
+    public static RequestSpecBuilder requestSpec(ContentType contentType) {
+        return requestSpec().setContentType(contentType);
+    }
+
+    public static RequestSpecBuilder requestSpec(String contentType) {
+        return requestSpec().setContentType(contentType);
+    }
+
+    public static RequestSpecBuilder requestSpec(Map<String, String> headers) {
+        return requestSpec().addHeaders(headers);
+    }
+
+    public static RequestSpecBuilder requestSpec(String headerName, String headerValue) {
+        return requestSpec().addHeader(headerName, headerValue);
+    }
+
+    public static RequestSpecBuilder requestSpec(ContentType contentType, Map<String, String> headers) {
+        return requestSpec(contentType).addHeaders(headers);
+    }
+
+    public static RequestSpecBuilder requestSpec(String contentType, Map<String, String> headers) {
+        return requestSpec(contentType).addHeaders(headers);
+    }
+
+    public static RequestSpecBuilder requestSpec(ContentType contentType, String headerName, String headerValue) {
+        return requestSpec(contentType).addHeader(headerName, headerValue);
+    }
+
+    public static RequestSpecBuilder requestSpec(String contentType, String headerName, String headerValue) {
+        return requestSpec(contentType).addHeader(headerName, headerValue);
+    }
+
+    public static RequestSpecBuilder requestSpec(WebBaseConfig config, ContentType contentType) {
+        return requestSpec(config).setContentType(contentType);
+    }
+
+    public static RequestSpecBuilder requestSpec(WebBaseConfig config, String contentType) {
+        return requestSpec(config).setContentType(contentType);
+    }
+
+    public static RequestSpecBuilder requestSpec(WebBaseConfig config, Map<String, String> headers) {
+        return requestSpec(config).addHeaders(headers);
+    }
+
+    public static RequestSpecBuilder requestSpec(WebBaseConfig config, String headerName, String headerValue) {
+        return requestSpec(config).addHeader(headerName, headerValue);
+    }
+
+    public static RequestSpecBuilder requestSpec(WebBaseConfig config, ContentType contentType, Map<String, String> headers) {
+        return requestSpec(config, contentType).addHeaders(headers);
+    }
+
+    public static RequestSpecBuilder requestSpec(WebBaseConfig config, String contentType, Map<String, String> headers) {
+        return requestSpec(config, contentType).addHeaders(headers);
+    }
+
+    public static RequestSpecBuilder requestSpec(WebBaseConfig config, ContentType contentType, String headerName, String headerValue) {
+        return requestSpec(config, contentType).addHeader(headerName, headerValue);
+    }
+
+    public static RequestSpecBuilder requestSpec(WebBaseConfig config, String contentType, String headerName, String headerValue) {
+        return requestSpec(config, contentType).addHeader(headerName, headerValue);
     }
 }
--- modules/utils/src/main/java/org/project/utils/request/Request.java
+++ modules/utils/src/main/java/org/project/utils/request/Request.java
@@ -1,33 +1,31 @@
 package org.project.utils.request;
 
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import io.restassured.specification.QueryableRequestSpecification;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.SpecificationQuerier;
-import jdk.jfr.Description;
-import org.project.utils.constant.RequestConstants.METHOD;
-
 import java.beans.ConstructorProperties;
 import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.net.*;
+import java.util.Map;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+
+import io.restassured.http.*;
+import io.restassured.response.Response;
+import io.restassured.specification.*;
+import jdk.jfr.Description;
+
 import static org.project.utils.Helper.debug;
-import static org.project.utils.config.ApiConfig.requestSpec;
-import static org.project.utils.constant.RequestConstants.METHOD.POST;
-import static org.project.utils.constant.RequestConstants.METHOD.PUT;
+import static org.project.utils.config.ApiConfig.*;
+import static org.project.utils.constant.RequestConstants.METHOD.*;
 import static org.project.utils.fs.FS.path;
 import static org.project.utils.reflection.Reflection.*;
+
+import org.project.utils.constant.RequestConstants.METHOD;
 
 public class Request {
 
     private RequestSpecification request;
     private final METHOD method;
     private final String methodSend;
+    private final String path;
     private final String url;
     private final URL URL;
     private final URI URI;
@@ -36,8 +34,10 @@ public class Request {
 
     @ConstructorProperties({})
     public Request() {
+        request = null;
         method = null;
         methodSend = null;
+        path = null;
         url = null;
         URL = null;
         URI = null;
@@ -46,10 +46,11 @@ public class Request {
 
     @ConstructorProperties({"method", "pathList"})
     public Request(METHOD method, Object... pathList) throws MalformedURLException, URISyntaxException {
-        request = given(requestSpec());
+        //request = givenRequestSpec();
+        request = getRequestSpec();
         this.method = method;
         methodSend = method.toString().toLowerCase();
-        String path = path(pathList);
+        path = path(pathList);
         request.basePath(path); // задаем базовый путь для запроса
         url = fullPath();
         URL = new URL(url);
@@ -68,17 +69,65 @@ public class Request {
         return invoke(request, methodSend, URL); // вызов метода с аргументами
     }
 
+    @Description("Builder: set body request")
+    public Request body(Object body) {
+        this.body = body;
+        return this;
+    }
+
     @Description("Builder: get response")
     public Response response() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-        request = request.contentType(ContentType.JSON); // header
+        contentType(JSON); // header
         request = (body != null && (method == POST || method == PUT) ? request.body(body) : request) // body json
             .when();
         return send().andReturn();
     }
 
-    @Description("Builder: set body request")
-    public Request body(Object body) {
-        this.body = body;
+    @Description("Builder: set content type")
+    public Request contentType(ContentType contentType) {
+        request = request.contentType(contentType);
+        return this;
+    }
+
+    @Description("Builder: set content type")
+    public Request contentType(String contentType) {
+        request = request.contentType(contentType);
+        return this;
+    }
+
+    @Description("Builder: no content type")
+    public Request noContentType() {
+        request = request.noContentType();
+        return this;
+    }
+
+    @Description("Builder: set headers")
+    public Request headers(Headers headers) {
+        request = request.headers(headers);
+        return this;
+    }
+
+    @Description("Builder: set headers")
+    public Request headers(Map<String, ?> map) {
+        request = request.headers(map);
+        return this;
+    }
+
+    @Description("Builder: set headers")
+    public Request headers(String s, Object o, Object... objects) {
+        request = request.headers(s, o, objects);
+        return this;
+    }
+
+    @Description("Builder: set header")
+    public Request header(Header header) {
+        request = request.header(header);
+        return this;
+    }
+
+    @Description("Builder: set header")
+    public Request header(String s, Object o, Object... objects) {
+        request = request.header(s, o, objects);
         return this;
     }
 
