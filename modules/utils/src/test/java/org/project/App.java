a8e49eb8f98278976e0eb9a86086c2eed7474285
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-18 19:41:55 +0300

    Update tests

--- modules/utils/src/test/java/org/project/App.java
+++ modules/utils/src/test/java/org/project/App.java
@@ -1,12 +1,16 @@
 package org.project;
 
 import java.io.*;
+import java.net.URISyntaxException;
 
 import io.appium.java_client.windows.WindowsDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.WebElement;
+import org.project.utils.request.Request;
+
+import static org.project.utils.constant.RequestConstants.METHOD.GET;
 
 public class App {
     protected static WebDriver driver;
@@ -14,7 +18,12 @@ public class App {
     protected static ChromeDriver webDriver;
     protected static RemoteWebDriver remoteDriver;
 
-    public static void main(String[] args) throws IOException, IllegalAccessException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        //testMain();
+        testApi();
+    }
+
+    public static void testMain() throws IOException, IllegalAccessException, ClassNotFoundException {
         driver = org.project.utils.windriver.RemoteWebDriver.start();
         remoteDriver = org.project.utils.windriver.RemoteWebDriver.start();
         webDriver = org.project.utils.windriver.WebDriver.start();
@@ -22,6 +31,11 @@ public class App {
         org.project.utils.windriver.WinDriver.printCall();
         //org.project.utils.windriver.WebDriver.stop();
         //org.project.utils.windriver.WebDriver.quit();
+    }
+
+    public static void testApi() throws IOException, URISyntaxException {
+        Request req = new Request(GET, "path").uri("https://googlechromelabs.github.io/");
+        req.printFullPath();
     }
 
 }
