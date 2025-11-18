6d9390a8db9df15c24ea5c0f7e05dea0996cb066
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-18 13:35:56 +0300

    Update tests

--- modules/utils/src/test/java/org/project/App.java
+++ modules/utils/src/test/java/org/project/App.java
@@ -1,12 +1,21 @@
 package org.project;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
 import io.appium.java_client.windows.WindowsDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.WebElement;
+import org.project.utils.config.ApiConfig;
+import org.project.utils.constant.RequestConstants;
+import org.project.utils.request.Request;
+
+import static org.project.utils.config.ApiConfig.getRequestSpec;
+import static org.project.utils.constant.RequestConstants.METHOD.GET;
 
 public class App {
     protected static WebDriver driver;
@@ -14,7 +23,14 @@ public class App {
     protected static ChromeDriver webDriver;
     protected static RemoteWebDriver remoteDriver;
 
-    public static void main(String[] args) throws IOException, IllegalAccessException, ClassNotFoundException {
+    public static void main(String[] args)
+        throws IOException, IllegalAccessException, URISyntaxException, InvocationTargetException, NoSuchMethodException
+    {
+        //testMain();
+        testApi();
+    }
+
+    public static void testMain() throws IOException, IllegalAccessException, ClassNotFoundException {
         driver = org.project.utils.windriver.RemoteWebDriver.start();
         remoteDriver = org.project.utils.windriver.RemoteWebDriver.start();
         webDriver = org.project.utils.windriver.WebDriver.start();
@@ -22,5 +38,12 @@ public class App {
         org.project.utils.windriver.WinDriver.printCall();
         //org.project.utils.windriver.WebDriver.stop();
         //org.project.utils.windriver.WebDriver.quit();
+    }
+
+    public static void testApi()
+        throws MalformedURLException, URISyntaxException, InvocationTargetException, NoSuchMethodException, IllegalAccessException
+    {
+        new Request(GET, "path");
+        new Request(GET, "path").response();
     }
 }
