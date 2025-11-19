8cab609a574b87c67920c4f797538cc1c2318200
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-19 13:31:56 +0300

    Update utils to v.4.10.3

--- modules/utils/src/test/java/org/project/App.java
+++ modules/utils/src/test/java/org/project/App.java
@@ -1,15 +1,24 @@
 package org.project;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
 
 import io.appium.java_client.windows.WindowsDriver;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.WebElement;
+import org.project.utils.json.JsonSchema;
 import org.project.utils.request.Request;
 
+import static org.project.utils.Helper.debug;
+import static org.project.utils.Helper.toList;
 import static org.project.utils.constant.RequestConstants.METHOD.GET;
 
 public class App {
@@ -18,9 +27,10 @@ public class App {
     protected static ChromeDriver webDriver;
     protected static RemoteWebDriver remoteDriver;
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException, URISyntaxException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
         //testMain();
-        testApi();
+        //testApi();
+        testJson();
     }
 
     public static void testMain() throws IOException, IllegalAccessException, ClassNotFoundException {
@@ -38,4 +48,38 @@ public class App {
         req.printFullPath();
     }
 
+    public static void testJson() throws IOException, URISyntaxException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        String uri = "https://googlechromelabs.github.io/chrome-for-testing/142.0.7444.61.json";
+
+        Request req = new Request(GET).uri(uri);
+        String jsonStr = req.asString();
+        debug(req.asPrettyString());
+
+        JsonSchema json = new JsonSchema(jsonStr);
+        String version = json.get("version", "string");
+        JSONArray chromedriver = json.get("downloads.chromedriver", "array");
+        Map<String, Object> map = json.toMap();
+
+        debug(version);
+        debug(chromedriver);
+        debug(map);
+
+        List<JSONObject> list = toList(chromedriver, o -> o.get("platform").equals("win64"));
+        debug(list);
+
+        Map<String, Object> map0 = list.get(0).toMap();
+        debug(map0);
+        debug(map0.get("url"));
+        debug(map.get("downloads"));
+
+        debug(json.toMap("downloads"));
+
+        List<JSONObject> list1 = json.toList("downloads.chromedriver", o -> o.get("platform").equals("win64"));
+        debug(list1);
+        debug(json.toList("downloads.chromedriver", "platform", "win64"));
+
+        List<JSONObject> list2 = JsonSchema.toList(chromedriver, o -> o.get("platform").equals("win64"));
+        debug(list2);
+        debug(JsonSchema.toList(chromedriver, "platform", "win64"));
+    }
 }
