0cd1f1f907e81c88e44d41ef2821641b65d8c24b
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-21 16:04:17 +0300

    Update utils to v.4.12.0

--- modules/utils/src/test/java/org/project/App.java
+++ modules/utils/src/test/java/org/project/App.java
@@ -3,7 +3,7 @@ package org.project;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
-import java.nio.file.*;
+import java.nio.file.Path;
 import java.util.*;
 
 import io.appium.java_client.windows.WindowsDriver;
@@ -17,7 +17,8 @@ import static org.project.utils.Helper.*;
 import static org.project.utils.constant.RequestConstants.METHOD.GET;
 import static org.project.utils.fs.Attributes.printAttrs;
 import static org.project.utils.fs.FS.*;
-import static org.project.utils.fs.Zip.unzip;
+import static org.project.utils.fs.Zip.*;
+import static org.project.utils.json.JsonSchema.jsonSchema;
 
 import org.project.utils.json.JsonSchema;
 import org.project.utils.request.Request;
@@ -27,11 +28,15 @@ public class App {
     protected static WindowsDriver<WebElement> winDriver;
     protected static ChromeDriver webDriver;
     protected static RemoteWebDriver remoteDriver;
+    protected static String ver = "142.0.7444.61";
+    protected static String uri = "https://googlechromelabs.github.io/";
+    protected static String endpoint = "chrome-for-testing/" + ver + ".json";
 
     public static void main(String[] args) throws IOException, URISyntaxException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
         //testMain();
         //testApi();
-        testJson();
+        //testJson();
+        testZip();
     }
 
     public static void testMain() throws IOException, IllegalAccessException, ClassNotFoundException {
@@ -50,16 +55,13 @@ public class App {
     }
 
     public static void testJson() throws IOException, URISyntaxException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-        String ver = "142.0.7444.61";
-        String uri = "https://googlechromelabs.github.io/";
-        String endpoint = "chrome-for-testing/" + ver + ".json";
+        JsonSchema json = jsonSchema(endpoint, uri);
+        Request req = json.req();
 
-        Request req = new Request(GET, endpoint).uri(uri);
-        String jsonStr = req.string();
-
+        debug(req.string());
         debug(req.pretty());
+        debug(req.statusCode());
 
-        JsonSchema json = new JsonSchema(jsonStr);
         String version = json.get("version", "string");
         JSONArray chromedriver = json.get("downloads.chromedriver", "array");
         Map<String, Object> map = json.toMap();
@@ -85,10 +87,21 @@ public class App {
 
         debug(json.arrayToMap("downloads.chromedriver"));
         debug(json.toMap("downloads.chromedriver", o -> ((JSONObject) o).get("platform").equals("win64")));
+
+        new Request(GET, "path", 1).uri("https://googlechromelabs.github.io/");
+
         debug(map0);
         debug(url);
-
         debug(path(url));
+    }
+
+    public static void testZip() throws IOException, URISyntaxException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        JsonSchema json = jsonSchema(endpoint, uri);
+        Request req = json.req();
+
+        Map<String, Object> map0 = json.toMap("downloads.chromedriver", "platform", "win64");
+        String url = (String) map0.get("url");
+        debug(url);
 
         Request req1 = new Request(GET).uri(url);
 
@@ -96,10 +109,9 @@ public class App {
         byte[] bytes = req1.bytes();
         String str = req1.string();
 
-        debug(str);
-        debug(req1.pretty());
+        //debug(str);
+        //debug(req1.pretty());
 
-        debug(req.statusCode());
         debug(req1.statusCode());
         debug(req1.statusCode());
 
@@ -108,29 +120,61 @@ public class App {
         String last = last(url, "/");
         debug(last);
 
-        writeFile(last, inputStream);
-        writeFile("filename.zip", bytes);
-        writeFile("filename.txt", str);
-        writeFile("filename.txt", req.string() + "\n" + req.pretty());
+        String out = "test/zip/";
+        String name = "filename";
+        String filename = out + name;
+        String filenameZip = filename + ".zip";
+        String filenameTxt = filename + ".txt";
+        String chromedriverRoot = "lib/chromedriver";
+        String chromedriverFile = "chromedriver.exe";
+        Path chromedriverPath = pathStr(chromedriverRoot, chromedriverFile);
 
-        debug(Path.of("lib/chromedriver", "chromedriver.exe"));
-        debug(Paths.get("lib/chromedriver", "chromedriver.exe"));
-        debug(Paths.get("lib/chromedriver", "chromedriver.exe").toString());
-        debug(new File(Paths.get("lib/chromedriver", "chromedriver.exe").toString()));
-        debug(pathStr("lib/chromedriver", "chromedriver.exe"));
-        debug(pathOf("lib/chromedriver", "chromedriver.exe"));
-
-        new Request(GET, "path", 1).uri("https://googlechromelabs.github.io/");
+        writeFile(out + last, inputStream);
+        writeFile(filenameZip, bytes);
+        writeFile(filenameTxt, str);
+        writeFile(filenameTxt, req.string() + "\n" + "\n" + req.pretty());
 
         printAttrs();
-        printAttrs("lib/chromedriver/chromedriver.exe");
+        printAttrs(chromedriverPath);
 
-        debug(req.string());
-        debug(req.pretty());
+        Path resolve = resolve(out);
+        Path resolve1 = resolve(out, out);
+        debug(resolve);
+        debug(resolve1);
 
-        org.openqa.selenium.io.Zip.unzip();
-        unzip();
-        unzip();
-        unzip();
+        debug(absolute(filenameZip).isAbsolute());
+        debug(absolute(out).isAbsolute());
+
+        debug(absolute(filenameZip));
+        debug(absolute(out));
+
+        debug(isDir(filenameZip));
+        debug(isFile(filenameZip));
+        debug(isDir(out));
+        debug(isFile(out));
+
+        debug(isDir(pathStr(filenameZip)) ? pathStr(filenameZip) : pathStr(filenameZip).getParent());
+        debug(isDir(pathStr(out)) ? pathStr(out) : pathStr(out).getParent());
+
+        debug(pathStr(filenameZip).getParent());
+        debug(pathStr(out).getParent());
+
+        debug(mkdirs(out));
+        debug(mkdirs(filenameZip));
+        debug(mkdirs("testDir/1"));
+
+        debug(new File(out).toString());
+
+        debug(readDir("src/test"));
+        debug(readDir("src/test").toList());
+
+        debug(delete("filename3.zip"));
+        debug(delete("test"));
+        debug(delete("testDir"));
+
+        unzip(filenameZip, out);
+        unzipFile(filenameZip, out + "1");
+        unzipPass(filenameZip, out + "2");
+        unzipSelenium(filenameZip, out + "3");
     }
 }
