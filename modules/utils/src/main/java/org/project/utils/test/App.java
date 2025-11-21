4a28ba3f015895cc4ae5d3619ef4c4a7bdc61567
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-21 17:33:07 +0300

    Update testApi

--- modules/utils/src/main/java/org/project/utils/test/App.java
+++ modules/utils/src/main/java/org/project/utils/test/App.java
@@ -5,11 +5,14 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
 
 import io.appium.java_client.windows.WindowsDriver;
+import io.restassured.response.Response;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import static org.project.utils.Helper.debug;
 import static org.project.utils.constant.RequestConstants.METHOD.GET;
+import static org.testng.Assert.assertEquals;
 
 import org.project.utils.request.Request;
 
@@ -36,9 +39,35 @@ public class App extends TestZip {
         //org.project.utils.windriver.WebDriver.quit();
     }
 
-    public static void testApi() throws IOException, URISyntaxException {
-        Request req = new Request(GET).uri(TestJson.uri);
+    public static void testApi() throws IOException, URISyntaxException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Response resp1 = new Request(GET, "store/order", 0).response();
+
+        debug(resp1);
+        debug(resp1.asString());
+        debug(resp1.asPrettyString());
+        debug(resp1.asInputStream());
+        debug(resp1.asByteArray());
+        debug(resp1.contentType());
+        debug(resp1.headers());
+        debug(resp1.sessionId());
+        debug(resp1.cookies());
+        debug(resp1.detailedCookies());
+
+        Request req1 = new Request(GET, "store/order", 1);
+        String resp2 = req1.string();
+
+        debug(req1);
+        debug(resp2);
+
+        Request req = new Request(GET).uri(uri);
+        Response resp = req.response();
         req.printFullPath();
+        req.printUrl();
+        debug(req.baseUri());
+        debug(resp);
+        debug(resp);
+
+        assertEquals(resp.getStatusCode(), 200);
     }
 
 }
