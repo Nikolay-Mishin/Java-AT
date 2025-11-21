6bb1b50985cf74dbb30d3653ba03fa78f7e668d2
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-21 21:26:52 +0300

    Add testAttrs

---
+++ modules/utils/src/main/java/org/project/utils/test/TestFS.java
@@ -0,0 +1,62 @@
+package org.project.utils.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.Files.readAttributes;
+import static org.project.utils.Helper.debug;
+import static org.project.utils.Helper.notNull;
+import static org.project.utils.fs.Attributes.attrs;
+import static org.project.utils.fs.Attributes.printAttrs;
+import static org.project.utils.fs.File.delete;
+import static org.project.utils.fs.File.exist;
+
+public class TestFS extends App {
+
+    public static void main(String[] args) throws IOException {
+        //testFS();
+        testAttrs();
+    }
+
+    public static void testFS() throws IOException {
+        for (FileStore store: FileSystems.getDefault().getFileStores()) {
+            long total = store.getTotalSpace() / 1024;
+            long used = (store.getTotalSpace() - store.getUnallocatedSpace()) / 1024;
+            long avail = store.getUsableSpace() / 1024;
+            System.out.format("%-20s %12d %12d %12d%n", store, total, used, avail);
+        }
+
+        debug(FileSystems.getDefault().getRootDirectories());
+        debug(FileSystems.getDefault().getRootDirectories());
+        debug(FileSystems.getDefault().getRootDirectories());
+        debug(FileSystems.getDefault().getPath("lib/chromedriver"));
+        debug(FileSystems.getDefault().getPath("lib/chromedriver").getFileSystem());
+        debug(FileSystems.getDefault().getPath("lib/chromedriver").getFileSystem().getRootDirectories());
+        debug(FileSystems.getDefault().getPath("lib/chromedriver").getFileSystem().getFileStores());
+        debug(FileSystems.getDefault().getPath("lib/chromedriver").getFileSystem().supportedFileAttributeViews());
+
+        debug(FileSystems.getDefault().getPath("lib/chromedriver/chromedriver.exe"));
+        debug(FileSystems.getDefault().getPath("lib/chromedriver/chromedriver.exe").toFile());
+        debug(FileSystems.getDefault().getPath("lib/chromedriver/chromedriver.exe").toFile().canExecute());
+
+        debug(Path.of("lib/chromedriver/chromedriver.exe"));
+        debug(Path.of("lib/chromedriver/chromedriver.exe").toFile());
+        debug(Path.of("lib/chromedriver/chromedriver.exe").toFile().canExecute());
+
+        debug(delete("filename1.zip"));
+        debug(delete(FileSystems.getDefault().getPath("filename.txt").toFile()));
+
+        debug(FileSystems.getDefault().supportedFileAttributeViews());
+    }
+
+    public static void testAttrs() {
+        debug(FileSystems.getDefault().supportedFileAttributeViews());
+        printAttrs();
+        printAttrs("home/user/example.txt");
+        printAttrs("/home/user/example.txt");
+        printAttrs("filename.zip");
+    }
+
+}
