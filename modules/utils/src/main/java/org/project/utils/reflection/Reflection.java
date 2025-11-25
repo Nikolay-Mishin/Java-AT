80a198659f1dcda282bdf60a32be5aef8aa89ba7
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-25 11:43:45 +0300

    Add get/set Field & get modifiers

--- modules/utils/src/main/java/org/project/utils/reflection/Reflection.java
+++ modules/utils/src/main/java/org/project/utils/reflection/Reflection.java
@@ -3,6 +3,7 @@ package org.project.utils.reflection;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.*;
 import java.util.*;
+import java.util.function.Function;
 
 import static org.apache.commons.beanutils.PropertyUtils.*;
 
@@ -74,7 +75,7 @@ public class Reflection {
 
     @Description("Get method of object")
     private static Method _getMethod(String get, Object obj, String method, Object... args) throws NoSuchMethodException {
-        Class<?> clazz = _getClass(obj);
+        Class<?> clazz = getClass(obj);
         Method _method;
         boolean declared = Objects.equals(get, "declared");
         get = declared ? "getDeclaredMethod" : "getMethod";
@@ -109,7 +110,7 @@ public class Reflection {
     }
 
     public static Class<?> getPrimitiveType(Object obj) {
-        Class<?> clazz = isInstance(obj, List.class) ? List.class : _getClass(obj);
+        Class<?> clazz = isInstance(obj, List.class) ? List.class : getClass(obj);
         String name = getClassSimpleName(clazz);
         return switch (name) {
             case ("Boolean") -> boolean.class;
@@ -123,20 +124,20 @@ public class Reflection {
         };
     }
 
-    public static Class<?> _getClass(Object obj) {
+    public static Class<?> getClass(Object obj) {
         Class<?> clazz = isClass(obj) ? (Class<?>) obj : obj.getClass();
         debug(clazz);
         return clazz;
     }
 
     public static String getClassName(Object obj) {
-        String name = _getClass(obj).getName();
+        String name = getClass(obj).getName();
         debug(name);
         return name;
     }
 
     public static String getClassSimpleName(Object obj) {
-        String name = _getClass(obj).getSimpleName();
+        String name = getClass(obj).getSimpleName();
         debug(name);
         return name;
     }
@@ -272,7 +273,7 @@ public class Reflection {
 
     @Description("Get property descriptors")
     public static List<PropertyDescriptor> getPropDescriptors(Object obj) {
-        PropertyDescriptor[] descList = getPropertyDescriptors(_getClass(obj));
+        PropertyDescriptor[] descList = getPropertyDescriptors(getClass(obj));
         debug(Arrays.toString(descList));
         return List.of(descList);
     }
@@ -347,6 +348,99 @@ public class Reflection {
         debug(_type == type);
         debug(method);
         return invoke(type, method, value); // вызов метода с аргументами
+    }
+
+    @Description("Get field")
+    public static Field field(Object obj, String name) throws NoSuchFieldException {
+        return field(getClass(obj), name);
+    }
+
+    //Если есть менеджер безопасности, это не сработает. Вам нужно обернуть вызов setAccessible и getDeclaredField в PriviledgedAction и запустить его через java.security.AccessController.doPrivileged(...)
+    @Description("Get field")
+    public static Field field(Class<?> clazz, String name) throws NoSuchFieldException {
+        try {return clazz.getDeclaredField(name);}
+        catch (NoSuchFieldException e) {
+            Class<?> superClass = clazz.getSuperclass();
+            if (superClass == null) throw e;
+            else return field(superClass, name);
+        }
+    }
+
+    @Description("Get field value")
+    public static Object getField(Object obj, String name) throws NoSuchFieldException, IllegalAccessException {
+        return field(obj, name, field -> {
+            try {return field.get(obj);}
+            catch (IllegalAccessException e) {throw new RuntimeException(e);}
+        });
+    }
+
+    @Description("Set field value")
+    public static Object setField(Object obj, String name, Object value) throws NoSuchFieldException, IllegalAccessException {
+        return field(obj, name, field -> {
+            try {
+                field.set(obj, value);
+                return field.get(obj);
+            } catch (IllegalAccessException e) {throw new RuntimeException(e);}
+        });
+    }
+
+    @Description("Set field value")
+    public static Object field(Object obj, String name, Function<Field, Object> cb) throws NoSuchFieldException, IllegalAccessException {
+        Field field = field(obj, name);
+        boolean makeAccessible = makeAccessible(field, obj);
+        debug("field: " + field);
+        Object value = cb.apply(field);
+        makeUnAccessible(field, makeAccessible);
+        return value;
+    }
+
+    @Description("Field set accessible to true")
+    public static boolean makeAccessible(Field field, Object obj) {
+        boolean makeAccessible = !canAccess(field, obj);
+        if (makeAccessible) field.setAccessible(true);
+        return makeAccessible;
+    }
+
+    @Description("Field set accessible to true")
+    public static boolean makeAccessible(Field field) {
+        boolean makeAccessible = !isPublic(field);
+        if (!isPublic(field)) field.setAccessible(true);
+        return makeAccessible;
+    }
+
+    @Description("Field set accessible to false")
+    public static void makeUnAccessible(Field field, boolean makeAccessible) {
+        if (makeAccessible) field.setAccessible(false);
+    }
+
+    @Description("Field set accessible to false")
+    public static void makeUnAccessible(Field field) {
+        field.setAccessible(false);
+    }
+
+    @Description("Field is static")
+    public static boolean isStatic(Field field) {
+        return Modifier.isStatic(field.getModifiers()) || Modifier.isStatic(field.getDeclaringClass().getModifiers());
+    }
+
+    @Description("Field is public")
+    public static boolean isPublic(Field field) {
+        return Modifier.isPublic(field.getModifiers()) || Modifier.isPublic(field.getDeclaringClass().getModifiers());
+    }
+
+    @Description("Field is protected")
+    public static boolean isProtected(Field field) {
+        return Modifier.isProtected(field.getModifiers()) || Modifier.isProtected(field.getDeclaringClass().getModifiers());
+    }
+
+    @Description("Field is private")
+    public static boolean isPrivate(Field field) {
+        return Modifier.isPrivate(field.getModifiers()) || Modifier.isPrivate(field.getDeclaringClass().getModifiers());
+    }
+
+    @Description("Field is accessible by object")
+    public static boolean canAccess(Field field, Object obj) {
+        return field.canAccess(isStatic(field) ? null : obj);
     }
 
 }
