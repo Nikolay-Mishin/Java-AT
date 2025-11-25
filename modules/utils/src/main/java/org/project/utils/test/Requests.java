03e768efc39e3f51cc16c6febd805368b61a6497
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-25 19:41:49 +0300

    Remove spaces

--- modules/utils/src/main/java/org/project/utils/test/Requests.java
+++ modules/utils/src/main/java/org/project/utils/test/Requests.java
@@ -3,9 +3,7 @@ package org.project.utils.test;
 import org.project.utils.request.BaseRequests;
 
 public class Requests extends BaseRequests<Order> {
-
     public Requests() {
         super("store/order");
     }
-
 }
