fa737b93cb538966ebd49b7609f0a809c3cf3e6c
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-25 11:42:10 +0300

    Refactor instance

--- modules/utils/src/main/java/org/project/utils/reflection/SingleInstance.java
+++ modules/utils/src/main/java/org/project/utils/reflection/SingleInstance.java
@@ -2,28 +2,38 @@ package org.project.utils.reflection;
 
 import java.lang.reflect.InvocationTargetException;
 
-import static org.project.utils.Helper.notNull;
-import static org.project.utils.reflection.Reflection.getGenericClass;
+import static org.project.utils.Helper.*;
+import static org.project.utils.reflection.Reflection.*;
 
 public class SingleInstance<T> extends Instance<T> {
 
     protected static SingleInstance<?> instance;
 
+    public static <T extends SingleInstance<?>> T instance() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+        //debug("instanceChildCall: " + getCallingChildClass());
+        //debug("generic: " + getGenericClass());
+        try {return (T) getField(getGenericClass(), "instance");}
+        catch (IllegalArgumentException e) {return (T) instance;}
+    }
+
     public static <T extends SingleInstance<?>> T instance(Object... args)
-        throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException
-    {
+        throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, NoSuchFieldException {
         return _instance(getGenericClass(), args);
     }
 
     public static <T extends SingleInstance<?>> T instance(Class<T> clazz, Object... args)
-        throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchFieldException {
         return _instance(clazz, args);
     }
 
     @SuppressWarnings("unchecked")
     protected static <T extends SingleInstance<?>> T _instance(Class<T> clazz, Object... args)
-        throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ClassNotFoundException {
-        return (T) (notNull(instance) ? instance : (instance = create(clazz, args)));
+        throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchFieldException {
+        debug("instance: " + clazz);
+        T instance = (T) getField(clazz, "instance");
+        debug("field.get: " + instance);
+        //return (T) (notNull(instance) ? instance : (instance = create(clazz, args)));
+        return notNull(instance) ? instance : (T) setField(clazz, "instance", create(clazz, args));
     }
 
 }
