62e3a18c119f0fb5e25600cc7fbef779eee6cff2
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-25 11:39:20 +0300

    Add instance

--- modules/utils/src/main/java/org/project/utils/test/TestAuth.java
+++ modules/utils/src/main/java/org/project/utils/test/TestAuth.java
@@ -2,28 +2,29 @@ package org.project.utils.test;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.file.FileStore;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 
 import static org.project.utils.Helper.debug;
-import static org.project.utils.auth.Auth.init;
-import static org.project.utils.fs.Attributes.printAttrs;
-import static org.project.utils.fs.File.delete;
-import static org.project.utils.fs.File.pathStr;
+import static org.project.utils.auth.Auth.*;
+
+import org.project.utils.reflection.SingleInstance;
 
 public class TestAuth {
 
     public static void main(String[] args)
-        throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException
+        throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException, NoSuchFieldException
     {
         testAuth();
     }
 
     public static void testAuth()
-        throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException
+        throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException, NoSuchFieldException
     {
-        init("auth", "token");
+        new Step();
+        init("auth/token");
+        debug("instance: " + instance());
+        debug("Auth: " + auth());
+        debug("SingleInstance: " + SingleInstance.instance());
+        printTokens();
     }
 
 }
