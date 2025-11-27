1f84c5eb7db9fd5a64e13533ed77cd5c35a96198
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-27 19:41:32 +0300

    Add TestException

---
+++ modules/utils/src/main/java/org/project/utils/test/TestException.java
@@ -0,0 +1,57 @@
+package org.project.utils.test;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.*;
+
+import org.junit.Test;
+
+import static org.project.utils.Helper.debug;
+import static org.project.utils.exception.UtilException.*;
+
+import org.project.utils.Helper;
+
+public final class TestException {
+
+    @Test
+    public void testConsumerWithCheckedExceptions() throws ClassNotFoundException {
+        Stream.of("java.lang.Object", "java.lang.Integer", "java.lang.String")
+            .forEach(rethrowConsumer(className -> debug(Class.forName(className))));
+
+        Stream.of("java.lang.Object", "java.lang.Integer", "java.lang.String")
+            .forEach(rethrowConsumer(Helper::debug));
+    }
+
+    @Test
+    public void testFunctionWithCheckedExceptions() throws ClassNotFoundException {
+        List<Class> classes1
+            = Stream.of("Object", "Integer", "String")
+            .map(rethrowFunction(className -> Class.forName("java.lang." + className)))
+            .collect(Collectors.toList());
+
+        List<Class> classes2
+            = Stream.of("java.lang.Object", "java.lang.Integer", "java.lang.String")
+            .map(rethrowFunction(Class::forName))
+            .collect(Collectors.toList());
+    }
+
+    @Test
+    public void testSupplierWithCheckedExceptions() throws UnsupportedEncodingException {
+        Collector.of(
+            rethrowSupplier(() -> new StringJoiner(new String(new byte[]{77, 97, 114, 107}, "UTF-8"))),
+            StringJoiner::add, StringJoiner::merge, StringJoiner::toString);
+    }
+
+    @Test
+    public void testUncheckExceptionThrownByMethod() {
+        Class clazz1 = uncheck(() -> Class.forName("java.lang.String"));
+        Class clazz2 = uncheck(Class::forName, "java.lang.String");
+    }
+
+    @Test (expected = ClassNotFoundException.class)
+    public void testIfCorrectExceptionIsStillThrownByMethod() {
+        Class clazz3 = uncheck(Class::forName, "INVALID");
+    }
+
+}
