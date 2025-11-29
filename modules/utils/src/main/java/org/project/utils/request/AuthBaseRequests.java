74974809adeb0132a6c53c9da17c66814346f138
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-29 13:23:44 +0300

    Update throws

--- modules/utils/src/main/java/org/project/utils/request/AuthBaseRequests.java
+++ modules/utils/src/main/java/org/project/utils/request/AuthBaseRequests.java
@@ -1,7 +1,10 @@
 package org.project.utils.request;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
 public class AuthBaseRequests<T> extends BaseRequests<T> {
-    public AuthBaseRequests(String baseUrl) {
+    public AuthBaseRequests(String baseUrl) throws MalformedURLException, URISyntaxException {
         super(baseUrl);
     }
 }
