19338780cf08976afe31552ad3fdd665272a2ed8
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-29 13:24:59 +0300

    Update throws

--- modules/utils/src/main/java/org/project/utils/test/Requests.java
+++ modules/utils/src/main/java/org/project/utils/test/Requests.java
@@ -1,9 +1,12 @@
 package org.project.utils.test;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
 import org.project.utils.request.BaseRequests;
 
 public class Requests extends BaseRequests<Order> {
-    public Requests() {
+    public Requests() throws MalformedURLException, URISyntaxException {
         super("store/order");
     }
 }
