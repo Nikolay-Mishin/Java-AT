7bf7b151563201026df592f39042d2e9a3ff403c
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-29 13:23:01 +0300

    Update debug

--- modules/utils/src/main/java/org/project/utils/test/TestAuth.java
+++ modules/utils/src/main/java/org/project/utils/test/TestAuth.java
@@ -2,6 +2,7 @@ package org.project.utils.test;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
 
 import static org.project.utils.Helper.debug;
 import static org.project.utils.auth.Auth.*;
@@ -10,18 +11,25 @@ import org.project.utils.reflection.SingleInstance;
 
 public class TestAuth {
 
-    public static void main(String[] args) throws IOException, ReflectiveOperationException {
+    public static void main(String[] args) throws IOException, URISyntaxException, ReflectiveOperationException {
         testAuth();
     }
 
     public static void testAuth()
-        throws IOException, NoSuchFieldException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException
+        throws IOException, URISyntaxException, NoSuchFieldException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException
     {
         debug("testAuth");
         init("auth/token");
         printTokens();
         debug("instance: " + instance());
         debug("SingleInstance: " + SingleInstance.instance());
+        debug("req: " + req());
+        debug("auth: " + auth());
+        debug("authUrl: " + auth().url());
+        init("auth/tokens");
+        debug("req: " + req());
+        debug("auth: " + auth());
+        debug("authUrl: " + auth().url());
     }
 
 }
