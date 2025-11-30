d4b15bcfdbf14e3aedf78470ab8700ddfdd8c1e4
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-30 21:09:58 +0300

    Update spaces

--- modules/utils/src/main/java/org/project/utils/constant/RequestConstants.java
+++ modules/utils/src/main/java/org/project/utils/constant/RequestConstants.java
@@ -1,8 +1,6 @@
 package org.project.utils.constant;
 
 public interface RequestConstants {
-
     enum METHOD { GET, POST, PUT, PATCH, DELETE }
     enum METHOD_LOWER_CASE { get, post, put, patch, delete }
-
 }
