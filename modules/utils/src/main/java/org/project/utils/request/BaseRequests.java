95058e2b4ac35f530dc57d155adc72bde56682b3
Nikolay Mishin <nikmarqa@gmail.com>
2025-11-30 21:32:33 +0300

    Refactor init

--- modules/utils/src/main/java/org/project/utils/request/BaseRequests.java
+++ modules/utils/src/main/java/org/project/utils/request/BaseRequests.java
@@ -8,37 +8,55 @@ import jdk.jfr.Description;
 
 import static org.project.utils.Helper.debug;
 import static org.project.utils.constant.RequestConstants.METHOD.*;
+import static org.project.utils.reflection.Reflection.getField;
+import static org.project.utils.reflection.Reflection.invoke;
+
+import org.project.utils.constant.RequestConstants.METHOD_LOWER_CASE;
+import org.project.utils.function.FunctionWithExceptions;
 
 public class BaseRequests<T> {
     protected String baseUrl;
     protected Request post = new Request(POST);
     protected Request get = new Request(GET);
     protected Request put = new Request(PUT);
+    protected Request patch = new Request(PATCH);
     protected Request delete = new Request(DELETE);
 
-    public BaseRequests(String baseUrl) throws MalformedURLException, URISyntaxException {
+    public BaseRequests(String baseUrl) throws MalformedURLException, URISyntaxException, ReflectiveOperationException {
         init(baseUrl);
     }
 
     @Description("Init baseUrl")
-    public <R extends BaseRequests<T>> R init(String baseUrl) throws MalformedURLException, URISyntaxException {
+    public <R extends BaseRequests<T>> R init(String baseUrl) throws MalformedURLException, URISyntaxException, ReflectiveOperationException {
         debug("setBaseUrl: " + baseUrl);
         baseUrl(baseUrl);
-        post.baseUrl(baseUrl);
-        get.baseUrl(baseUrl);
-        put.baseUrl(baseUrl);
-        delete.baseUrl(baseUrl);
-        return (R) this;
+        //return init(req -> invoke(req, "baseUrl", baseUrl));
+        return init(req -> invoke(this, "baseUrl", req, baseUrl));
     }
 
     @Description("Set uri")
-    public <R extends BaseRequests<T>> R uri(String uri) throws MalformedURLException, URISyntaxException {
+    public <R extends BaseRequests<T>> R uri(String uri) throws MalformedURLException, URISyntaxException, ReflectiveOperationException {
         debug("setUri: " + uri);
-        post.uri(uri);
-        get.uri(uri);
-        put.uri(uri);
-        delete.uri(uri);
+        //return init(req -> invoke(req, "uri", uri));
+        return init(req -> invoke(this, "uri", req, uri));
+    }
+
+    @Description("Init with cb")
+    public <R extends BaseRequests<T>, V, E extends Exception> R init(FunctionWithExceptions<Request, V, E> cb)
+        throws MalformedURLException, URISyntaxException, NoSuchFieldException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, E {
+        for (METHOD_LOWER_CASE method : METHOD_LOWER_CASE.values())
+            cb.apply((Request) getField(this, method.toString()));
         return (R) this;
+    }
+
+    @Description("Set baseUrl request")
+    public Request baseUrl(Request req, String baseUrl) throws MalformedURLException, URISyntaxException {
+        return req.baseUrl(baseUrl);
+    }
+
+    @Description("Set uri request")
+    public Request uri(Request req, String uri) throws MalformedURLException, URISyntaxException {
+        return req.uri(uri);
     }
 
     @Description("Get baseUrl")
@@ -72,6 +90,16 @@ public class BaseRequests<T> {
     }
 
     @Description("Get PUT request")
+    public Request patch() {
+        return patch;
+    }
+
+    @Description("Set PATCH request")
+    public Request patch(Request req) {
+        return patch = req;
+    }
+
+    @Description("Get PUT request")
     public Request put() {
         return put;
     }
@@ -92,8 +120,8 @@ public class BaseRequests<T> {
     }
 
     @Description("Add a new object")
-    public Response post(T model) throws MalformedURLException, URISyntaxException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-        return post.body(model).response();
+    public Response post(T model) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        return post.response(model);
     }
 
     @Description("Find object by ID")
@@ -102,8 +130,13 @@ public class BaseRequests<T> {
     }
 
     @Description("Update object")
-    public Response put(T model) throws MalformedURLException, URISyntaxException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-        return put.body(model).response();
+    public Response put(T model) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        return put.response(model);
+    }
+
+    @Description("Patch object")
+    public Response patch(T model) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        return patch.response(model);
     }
 
     @Description("Delete object")
