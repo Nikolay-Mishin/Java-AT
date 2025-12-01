eb5c349e567d3255c205a1f918153585064560ab
Nikolay Mishin <nikmarqa@gmail.com>
2025-12-01 21:57:17 +0300

    Update properties

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -6,8 +6,12 @@ import static org.project.utils.config.WebConfig.*;
 
 import org.project.utils.config.WebBaseConfig;
 
-//@Sources({"classpath:${env}.properties"})
-@Sources({"classpath:stand.properties"})
+@LoadPolicy(LoadType.MERGE)
+@Sources({
+    "system:properties", // -DpropertyName=propertyValue
+    "classpath:dev.properties",
+    "classpath:stand.properties"
+})
 public interface WebConfig extends WebBaseConfig {
     WebConfig BASE_CONFIG = config(WebConfig.class);
     int DEBUG_LEVEL = debugLvl();
