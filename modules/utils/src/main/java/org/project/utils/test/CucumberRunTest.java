8e255b884f9c9a22d96f10a3da43238d249be58b
Nikolay Mishin <nikmarqa@gmail.com>
2025-12-05 18:53:42 +0300

    Update CucumberRunTest

--- modules/utils/src/main/java/org/project/utils/test/CucumberRunTest.java
+++ modules/utils/src/main/java/org/project/utils/test/CucumberRunTest.java
@@ -11,12 +11,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+import java.util.function.Function;
 
 import static io.cucumber.core.cli.Main.run;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.aeonbits.owner.Config.Sources;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
@@ -38,7 +38,6 @@ import org.project.utils.config.TestBaseConfig;
         //"org.project.utils.event.CucumberEventListener:config.WebConfig"
     }
 )
-@Sources({"classpath:cucumber.properties"})
 public class CucumberRunTest {
 
     public CucumberRunTest() {
@@ -50,21 +49,30 @@ public class CucumberRunTest {
         setOptions();
     }
 
-    public static void setOptions(String[] options) {
+    public static String[] setOptions(String[] options) {
+        return setOptions(options, o -> o);
+    }
+
+    public static String[] setCliOptions(String[] options) {
+        return setOptions(options, o -> run(options, currentThread().getContextClassLoader()));
+    }
+
+    public static <R> String[] setOptions(String[] options, Function<String[], R> cb) {
         out.println(Arrays.toString(options));
-        run(options, currentThread().getContextClassLoader());
+        cb.apply(options);
+        return options;
     }
 
-    public static void setOptions() {
-        setOptions(getOptions());
+    public static String[] setOptions() {
+        return setOptions(getOptions());
     }
 
-    public static void setOptions(TestBaseConfig config) throws ReflectiveOperationException {
-        setOptions(getOptions(config));
+    public static String[] setOptions(TestBaseConfig config) throws ReflectiveOperationException {
+        return setOptions(getOptions(config));
     }
 
-    public static void setOptions(String plugins) throws ReflectiveOperationException {
-        setOptions(getOptions(plugins));
+    public static String[] setOptions(String plugins) throws ReflectiveOperationException {
+        return setOptions(getOptions(plugins));
     }
 
     public static String[] getOptions() {
