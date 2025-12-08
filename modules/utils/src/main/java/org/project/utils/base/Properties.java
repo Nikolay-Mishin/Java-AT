7368e127a310d383c344552af7e7e96919a5ea1e
Nikolay Mishin <nikmarqa@gmail.com>
2025-12-08 17:16:59 +0300

    Refactor setProps

--- modules/utils/src/main/java/org/project/utils/base/Properties.java
+++ modules/utils/src/main/java/org/project/utils/base/Properties.java
@@ -15,6 +15,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 import static org.project.utils.Helper.last;
 import static org.project.utils.base.HashMap.newTreeMap;
@@ -26,84 +27,148 @@ import static org.project.utils.stream.InputStream.input;
 
 public class Properties extends java.util.Properties {
     protected static final Map<String, Map<Object, Object>> propsMap = new HashMap<>();
-    protected static Map<Object, Object> sortedProps = new HashMap<>();
     protected static final Properties props = new Properties();
+    protected static boolean setBaseProps = true;
+    protected static String basePropsPrefix = "org.project.utils.";
+    protected static List<String> baseProps = List.of("dev", "test", "web", "win");
     protected static String propsDir = "src/main/resources/";
     protected static String kPrefix = "props.";
-    protected Map<Object, Object> map = newTreeMap();
+    protected static String kBasePrefix = kPrefix + "utils.";
+    protected static String propsExt = ".properties";
+    protected final Map<Object, Object> map = newTreeMap();
 
-    public static Map<String, Map<Object, Object>> getPropsMap() {
+    public static Map<String, Map<Object, Object>> propsMap() {
         return propsMap;
     }
 
-    public static Map<Object, Object> getSortedProps() {
-        return sortedProps;
-    }
-
-    public static Properties getProps() {
+    public static Properties props() {
         return props;
     }
 
-    public static Properties setProps() {
-        if (props.isEmpty()) {
-            try {
-                List<Path> files = rootPathList(propsDir).toList();
-                out.println("read: " + files);
-                files.forEach(r -> {
-                    String file = r.getFileName().toString();
-                    String name = propsName(file);
-                    String last = last(name, "\\.");
-                    String key = kPrefix + last;
-                    setProp(key, file);
-                });
-                //sortedProps.putAll(props.sortedProps());
-                sortedProps = props.sortedProps();
-            } catch (IOException | ReflectiveOperationException e) {
-                e.printStackTrace();
-                //throw new RuntimeException(e);
-            }
-        }
-        return props;
+    public static boolean isSetBaseProps() {
+        return setBaseProps;
     }
 
-    public static String getPropsDir(String path) {
+    public static boolean setBaseProps(boolean setBaseProps) {
+        return Properties.setBaseProps = setBaseProps;
+    }
+
+    public static String basePropsPrefix() {
+        return basePropsPrefix;
+    }
+
+    public static String basePropsPrefix(String prefix) {
+        return basePropsPrefix = prefix;
+    }
+
+    public static String propsDir() {
         return propsDir;
     }
 
-    public static String setPropsDir(String path) {
+    public static String propsDir(String path) {
         return propsDir = path;
     }
 
-    public static String setProp(String key, String classpath) {
-        return setProp(propsDir, key, classpath);
+    public static List<String> baseProps() {
+        return baseProps;
     }
 
-    public static String setProp(String loadDir, String key, String classpath) {
-        String prop = "";
-        try {
-            Properties props = loadProps(loadDir, classpath);
-            prop = System.setProperty(key, "classpath:" + classpath);
-            propsMap.put(key, props.sortedProps());
-        } catch (IOException | ReflectiveOperationException e) {
-            e.printStackTrace();
-            //throw new RuntimeException(e);
+    public static List<String> baseProps(List<String> props) {
+        return baseProps = props;
+    }
+
+    public static List<String> baseProps(String... props) {
+        return baseProps(List.of(props));
+    }
+
+    public static String kPrefix() {
+        return kPrefix;
+    }
+
+    public static String kPrefix(String prefix) {
+        return kPrefix = prefix;
+    }
+
+    public static String kBasePrefix() {
+        return kBasePrefix;
+    }
+
+    public static String kBasePrefix(String prefix) {
+        return kBasePrefix = prefix;
+    }
+
+    public static String propsExt() {
+        return propsExt;
+    }
+
+    public static String propsExt(String ext) {
+        return propsExt = ext;
+    }
+
+    public static Properties setBaseProps() {
+        if (props.isEmpty()) {
+            out.println("baseProps: " + baseProps.stream().map(f -> basePropsPrefix + f + propsExt).toList());
+            String _kPrefix = kPrefix;
+            kPrefix(kBasePrefix);
+            setProps(baseProps, f -> basePropsPrefix + f + propsExt, Properties::loadPropsClasspath);
+            kPrefix(_kPrefix);
+            return props;
         }
+        return null;
+    }
+
+    public static Properties setProps() {
+        return setProps(propsDir);
+    }
+
+    public static Properties setProps(String loadDir) {
+        if (setBaseProps) setBaseProps();
+        try {
+            List<Path> files = rootPathList(propsDir).toList();
+            out.println("read: " + files);
+            setProps(files, f -> f.getFileName().toString(), cp -> loadProps(loadDir, cp));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return props;
+    }
+
+    public static <T> Properties setProps(List<T> files, Function<T, String> cb, Function<String, Properties> setProp) {
+        files.forEach(f -> setProp(cb.apply(f), setProp));
+        return props;
+    }
+
+    public static String setProp(String file, Function<String, Properties> props) {
+        String name = propsName(file);
+        String last = last(name, "\\.");
+        String key = kPrefix + last;
+        return setProp(props.apply(file), key, file);
+    }
+
+    public static String setProp(Function<String, Properties> props, String key, String classpath) {
+        return setProp(props.apply(classpath), key, classpath);
+    }
+
+    public static String setProp(Properties props, String key, String classpath) {
+        String prop = System.setProperty(key, "classpath:" + classpath);
+        propsMap.put(key, props);
         return prop;
     }
 
-    public static Properties loadProps(String classpath) throws IOException {
+    public static Properties loadProps(String classpath) {
         return loadProps(propsDir, classpath);
     }
 
-    public static Properties loadProps(String loadDir, String classpath) throws IOException {
+    public static Properties loadProps(String loadDir, String classpath) {
         Properties _props = loadPropsFile(path(loadDir, classpath));
         mergeProps(props, _props);
         return _props;
     }
 
-    public static Properties loadPropsFile(String path) throws IOException {
+    public static Properties loadPropsFile(String path) {
         Properties props = new Properties();
-        props.load(input(path));
+        try { props.loadPath(path); }
+        catch (IOException e) { e.printStackTrace(); }
         return props;
     }
 
@@ -113,7 +178,38 @@ public class Properties extends java.util.Properties {
     }
 
     public static String propsName(String classpath) {
-        return classpath.replace(".properties", "");
+        return classpath.replace(propsExt, "");
+    }
+
+    public static Properties loadPropsClasspath(String classpath) {
+        return loadProps(Properties.class, classpath, true);
+    }
+
+    public static Properties loadProps(Class<?> clazz, String classpath) {
+        return loadProps(clazz, classpath, true);
+    }
+
+    /**
+     * В случае загрузки ресурсов из текущего класса путь указывается относительно класса.
+     * <p>Для загрузки по абсолютным путям от корня classpath используется начальный слеш:
+     * <p>Например, {@code InputStream input = ClassName.class.getResourceAsStream("/com/example/file.properties")}.
+     * @param clazz
+     * @param classpath
+     */
+    public static Properties loadProps(Class<?> clazz, String classpath, boolean absolute) {
+        try (final InputStream stream = clazz.getResourceAsStream((absolute ? "/" : "") + classpath)) {
+            props.load(stream);
+        }
+        catch (IOException e) { e.printStackTrace(); }
+        return props;
+    }
+
+    public Map<Object, Object> map() {
+        return map;
+    }
+
+    public void map(Map<Object, Object> m) {
+        map.putAll(m);
     }
 
     /*
@@ -155,9 +251,10 @@ public class Properties extends java.util.Properties {
         }
     }
 
-    public void putAll(Properties p) {
+    public Properties putAll(Properties p) {
         super.putAll(p);
         map(p.map());
+        return props;
     }
 
     @Override
@@ -165,12 +262,17 @@ public class Properties extends java.util.Properties {
         map.forEach(action);
     }
 
-    public Map<Object, Object> map() {
-        return map;
+    public Properties loadPath(Path path) throws IOException {
+        return loadPath(path.toString());
     }
 
-    protected void map(Map<Object, Object> m) {
-        map.putAll(m);
+    public Properties loadPath(String path) throws IOException {
+        load(input(path));
+        return props;
+    }
+
+    public Properties load(String classpath) {
+        return loadProps(this.getClass(), classpath);
     }
 
     public Map<Object, Object> sortedProps() throws ReflectiveOperationException {
