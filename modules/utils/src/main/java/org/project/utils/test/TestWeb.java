7d5137af3dbc4256c7a7a8d6d65940851db830f0
Nikolay Mishin <nikmarqa@gmail.com>
2025-12-15 13:56:07 +0300

    Add TestWeb

---
+++ modules/utils/src/main/java/org/project/utils/test/TestWeb.java
@@ -0,0 +1,107 @@
+package org.project.utils.test;
+
+import java.net.MalformedURLException;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.openqa.selenium.JavascriptExecutor;
+
+import static org.project.utils.Helper.debug;
+import static org.project.utils.Helper.sb;
+import static org.project.utils.request.Request.getParams;
+import static org.project.utils.request.Request.getParamsSlash;
+import static org.project.utils.windriver.WebDriver.ls;
+
+/**
+ *
+ */
+public class TestWeb extends TestWinDriver {
+    /**
+     *
+     */
+    protected static String endpoint;
+    /**
+     *
+     */
+    protected static String tokenK;
+    /**
+     *
+     */
+    protected static String token;
+    /**
+     *
+     */
+    protected static String project;
+
+    /**
+     *
+     */
+    public static void init() {
+        debug("TestWeb:init");
+        TestWinDriver.init();
+        token = w.getToken();
+        endpoint = w.getEndpoint();
+        tokenK = w.getTokenKey();
+        project = url(w.getProjectId(), token);
+        debug("getParams: " + getParamsSlash(tokenK, token));
+    }
+
+    /**
+     *
+     * @param project int
+     * @param token String
+     * @return String
+     */
+    public static String url(int project, String token) {
+        return sb(endpoint, project, getParams(tokenK, token));
+    }
+
+    /**
+     *
+     * @throws ReflectiveOperationException throws
+     * @throws MalformedURLException throws
+     */
+    public static void testWeb() throws ReflectiveOperationException, MalformedURLException {
+        webDriver = org.project.utils.windriver.WebDriver.start(url);
+        assertNotNull(webDriver);
+
+        // Cast WebDriver to JavascriptExecutor
+        JavascriptExecutor js = webDriver;
+
+        // Execute JavaScript to retrieve item from localStorage
+        String setItemScript = "localStorage.setItem(arguments[0], arguments[1])";
+        String getItemsScript = "return JSON.stringify(localStorage);";
+        String getItemScript = "return localStorage.getItem('accessToken');";
+
+        //js.executeScript(setItemScript, "accessToken", "");
+        //js.executeScript(setItemScript, "refreshToken", "");
+        String localStorageData = (String) js.executeScript(getItemsScript);
+        String localStorageItem = (String) js.executeScript(getItemScript);
+        //webDriver.executeScript(setItemScript);
+
+        // Print the localStorage data
+        debug("localStorage item: " + localStorageItem);
+        debug("localStorage data: " + localStorageData);
+
+        ls.set("accessToken", "");
+        ls.set("refreshToken", "");
+
+        // Print the localStorage data
+        debug("localStorage item: " + ls.get("accessToken"));
+        debug("localStorage data: " + ls.items());
+
+        // Navigate to the webpage where localStorage data is stored
+        debug("project: " + project);
+        webDriver.get(project);
+
+        /*
+        assertFalse(driverWeb.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        driverWeb.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+        assertTrue(driverWeb.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        //count total number of checkbox
+        debug(driverWeb.findElements(By.cssSelector("input[type='checkbox']")).size());
+        */
+        //quit();
+    }
+
+}
