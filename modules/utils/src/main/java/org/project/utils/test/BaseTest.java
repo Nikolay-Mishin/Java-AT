66f2488ec5ea272ff0beccc0aafc6c9d387d85e3
Nikolay Mishin <nikmarqa@gmail.com>
2025-12-22 21:27:13 +0300

    Update config

--- modules/utils/src/main/java/org/project/utils/test/BaseTest.java
+++ modules/utils/src/main/java/org/project/utils/test/BaseTest.java
@@ -5,21 +5,34 @@ import java.beans.ConstructorProperties;
 import static org.project.utils.Helper.debug;
 import static org.project.utils.config.TestConfig.getConfig;
 
+import org.project.utils.config.DriverBaseConfig;
+import org.project.utils.config.DriverConfig;
 import org.project.utils.config.TestBaseConfig;
+import org.project.utils.config.WebBaseConfig;
+import org.project.utils.config.WebConfig;
 import org.project.utils.reflection.SingleInstance;
 
 /**
  * @param <T> extends TestBaseConfig
  */
-public class BaseTest<T extends TestBaseConfig> extends SingleInstance<BaseTest<T>> {
+public class BaseTest<T extends TestBaseConfig, W extends WebBaseConfig, D extends DriverBaseConfig> extends SingleInstance<BaseTest<T, W, D>>
+{
     /**
      *
      */
-    protected static BaseTest<? extends TestBaseConfig> i;
+    protected static BaseTest<? extends TestBaseConfig, ? extends WebBaseConfig, ? extends DriverBaseConfig> i = SingleInstance.instance();
     /**
      *
      */
-    protected T c;
+    protected T c = getConfig();
+    /**
+     *
+     */
+    protected W w = WebConfig.getConfig();
+    /**
+     *
+     */
+    protected D win = DriverConfig.getConfig();
 
     /**
      *
@@ -29,28 +42,36 @@ public class BaseTest<T extends TestBaseConfig> extends SingleInstance<BaseTest<
     @ConstructorProperties({})
     public BaseTest() throws NoSuchFieldException, IllegalAccessException {
         debug("BaseTest:init");
+        //setInstance(this);
+        debug("i: " + i);
+        debug("c: " + c);
+        debug("w: " + w);
+        debug("win: " + win);
         c = getConfig();
-        setInstance(this);
+        w = WebConfig.getConfig();
+        win = DriverConfig.getConfig();
+        debug("i: " + i);
+        debug("c: " + c);
+        debug("w: " + w);
+        debug("win: " + win);
+        debug("c: " + getConfig());
+        /*debug("instance: " + instance());
+        debug("c: " + c());
+        debug("w: " + w());
+        debug("win: " + win());*/
     }
 
     /**
      *
-     * @return BaseTest extends TestBaseConfig
-     * @throws ReflectiveOperationException throws
-     */
-    public static BaseTest<? extends TestBaseConfig> init() throws ReflectiveOperationException {
-        debug("BaseTest:instance");
-        return SingleInstance.instance();
-    }
-
-    /**
-     *
-     * @return BaseTest extends TestBaseConfig
+     * @return BaseTests {T, W, D}
+     * @param <T> extends TestBaseConfig
+     * @param <W> extends WebBaseConfig
+     * @param <D> extends DriverBaseConfig
      * @throws NoSuchFieldException throws
      * @throws ClassNotFoundException throws
      * @throws IllegalAccessException throws
      */
-    public static BaseTest<? extends TestBaseConfig> instance() throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
+    public static <T extends TestBaseConfig, W extends WebBaseConfig, D extends DriverBaseConfig> BaseTest<T, W, D> instance() {
         return SingleInstance.getInstance();
     }
 
@@ -62,6 +83,26 @@ public class BaseTest<T extends TestBaseConfig> extends SingleInstance<BaseTest<
     @SuppressWarnings("unchecked")
     public static <T extends TestBaseConfig> T c() {
         return (T) i.c;
+    }
+
+    /**
+     *
+     * @return T
+     * @param <T> extends WebBaseConfig
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends WebBaseConfig> T w() {
+        return (T) i.w;
+    }
+
+    /**
+     *
+     * @return T
+     * @param <T> extends DriverBaseConfig
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends DriverBaseConfig> T win() {
+        return (T) i.win;
     }
 
 }
