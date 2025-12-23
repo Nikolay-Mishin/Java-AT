b04c690d82d4e224dd5411a6e7e6de76ac060b20
Nikolay Mishin <nikmarqa@gmail.com>
2025-12-23 14:07:35 +0300

    Update utils to v.7.8.7

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,4 +1,12 @@
+import org.junit.BeforeClass;
+
 import org.project.utils.test.CucumberRunTest;
 
+import static config.TestConfig.BASE_CONFIG;
+
 public class ApiTest extends CucumberRunTest {
+    @BeforeClass
+    public static void setUp() throws ReflectiveOperationException {
+        setUp(BASE_CONFIG);
+    }
 }
